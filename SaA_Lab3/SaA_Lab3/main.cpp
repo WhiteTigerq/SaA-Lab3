edab30012ebabde5f9974600482b1d1535f873a4
Oleg <41596219+WhiteTigerq@users.noreply.github.com>
2019-12-19 11:57:47 +0300

    In

---
+++ SaA_Lab3/SaA_Lab3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SaA_Lab3
+//
+//  Created by Олег Джинганин on 19.12.2019.
+//  Copyright © 2019 Олег Джинганин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
